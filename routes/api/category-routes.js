d4802bc9520a851a98f60c99b387f8913ddb37bc
Rosebud <rkle408@gmail.com>
2022-10-07 19:26:49 -0700

    edited categorty route, added tag and product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -58,7 +58,7 @@ router.put('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category with this id!' });
       return;
     }
-    res.status(200).json(userData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
